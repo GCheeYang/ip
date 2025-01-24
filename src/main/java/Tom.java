3e1d690f37a0a7475e103623ee6165ec7fd43f3f
GCheeYang <>
2025-01-24 11:10:02 +0800

    Level 1-2

--- src/main/java/Tom.java
+++ src/main/java/Tom.java
@@ -1,19 +1,25 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Tom {
     public static void main(String[] args) {
-        String line = "________________________________________________\n";
+        String line = "\n________________________________________________\n";
         Scanner scanner = new Scanner(System.in);
+        List<String> tasks = new ArrayList<>();
 
-        System.out.println(line + "Hello! I'm Tom\n" + "What Can I do for you?\n" + line);
+        System.out.println(line + "Hello! I'm Tom\n" + "What Can I do for you?" + line);
 
         while(true) {
             String input = scanner.nextLine(); //takes in user input
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!\n" + line);
                 break;
+            } else if(input.equals("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(i+1 + ". " + tasks.get(i));
+                }
             } else {
-                System.out.println(input);
+                tasks.add(input);
+                System.out.println("added: " + input + line);
             }
         }
 
@@ -24,14 +30,3 @@ public class Tom {
 
 
 }
-
-/*
-public static void main(String[] args) {
-    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-    System.out.println("Enter username");
-
-    String userName = myObj.nextLine();  // Read user input
-    System.out.println("Username is: " + userName);  // Output user input
-}
-
- */
