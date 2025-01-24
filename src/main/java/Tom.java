8f8f4079631ba1376504d1eca1692760652cdc30
GCheeYang <>
2025-01-24 10:56:15 +0800

    Revert "Level 1-1"
    This reverts commit 8479005f048ffdb3d401e3c9d4a4e96ec39c9a44.

--- src/main/java/Tom.java
+++ src/main/java/Tom.java
@@ -24,3 +24,14 @@ public class Tom {
 
 
 }
+
+/*
+public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+    System.out.println("Enter username");
+
+    String userName = myObj.nextLine();  // Read user input
+    System.out.println("Username is: " + userName);  // Output user input
+}
+
+ */
