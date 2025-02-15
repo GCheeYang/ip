32c4e01b00b70dc7f463314bc32dd6f1fc95c294
GCheeYang <>
2025-02-15 18:13:29 +0800

    Level-9

--- src/main/java/Tom.java
+++ src/main/java/Tom.java
@@ -2,7 +2,7 @@ import Tom.tasks.*;
 import java.util.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-//comment
+
 public class Tom {
     Path filePath = Paths.get("data", "tom.txt");
     private static List<Task> tasks;
