6ba5ca57d9651d7a05be0f127ece852e2fd56eb3
GCheeYang <>
2025-02-24 13:36:44 +0800

    Merging PRs test

--- src/main/java/Tom/Tom.java
+++ src/main/java/Tom/Tom.java
@@ -7,6 +7,7 @@ import Tom.tasks.TaskList;
 public class Tom {
     private TaskList tasks;
     private Storage storage;
+    private String test;
 
     public Tom() {
         storage = new Storage();
